f55e57351356e6f10a21e89ed4514ccaf781be57
KatyaPopova <hadron@yandex.ru>
2025-03-27 22:34:39 +0300

    init

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        String govno = "Hello World puska!";
+        String govno2 = "Hello govnishe";
+        String govno3;
+        govno3 = ("pidor baba ebanaya");
+        System.out.println(govno);
+        System.out.println(govno2);
+        System.out.println(govno3);
+        System.out.println(govno + govno2);
+        System.out.println(govno + "pidor");
+    }
+}
